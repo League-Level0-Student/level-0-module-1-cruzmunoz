dfd7f3fa452cf46289c85335dd02ecbaa7483612
munoz.cruz <munoz.cruz@SEMS-259-06.SEMS259.LAB>
2017-11-29 14:57:05 -0800

    Unbirthday, Remarkable ip

---
+++ src/Unbirthday.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday 
+{
+	public static void main(String[] args) {
+	String birthday=JOptionPane.showInputDialog("what is your birthday");
+	if(birthday.equals("11/29")) {
+JOptionPane.showMessageDialog(null, "happy birthday");
+}
+	else  {
+		JOptionPane.showMessageDialog(null, "happy UNbirthday");
+	}
+	
+		
+	}}
+	
